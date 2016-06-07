3e21efa105eb46148497c692862b19694257b96c
JelasticJPS <ik+1@jelastic.com>
2016-06-07 15:52:45 +0300

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,52 +1,51 @@
 #!/bin/bash
-node=$1
-nodeGroup=$2
-nodeType=$3
-address=$4
-url=$5
-id=$6
-port=$7
-name=$8
+nodeGroup=$1
+nodeType=$2
+address=$3
+url=$4
+id=$5
+port=$6
+name=$7
 
-if [ "sqldb" == "${node.Group}" ]; then \
+if [ "sqldb" == "${nodeGroup}" ]; then \
    PASSWORD=`cat /var/log/jem.log | grep "passwd" | awk -F \ \ '{print $6}' | tail -n1` \
-   ; if [ "`expr index ${node.nodeType} mysql`" == "0" ]; then \
+   ; if [ "`expr index ${nodeType} mysql`" == "0" ]; then \
       nodeType=mysql \
       nodeTypeSQL=mysql \
       userName=root \
-      REPLACEMENT='db.${nodeType}.driverClass=org.${nodeTypeSQL}.Driver\ndb.${nodeType}.url=jdbc:${nodeTypeSQL}://${node.address}/\ndb.${nodeType}.username=${userName}\ndb.${nodeType}.password=${PASSWORD}' \
+      REPLACEMENT='db.${nodeType}.driverClass=org.${nodeTypeSQL}.Driver\ndb.${nodeType}.url=jdbc:${nodeTypeSQL}://${address}/\ndb.${nodeType}.username=${userName}\ndb.${nodeType}.password=${PASSWORD}' \
       sed "s~//StartJel~//StartJel\n${REPLACEMENT}\n~g" ${HOME}/mobileapp-server-api.properties \
    ; fi \
-   ; if [ "`expr index ${node.nodeType} maria`" == "0" ]; then \
+   ; if [ "`expr index ${nodeType} maria`" == "0" ]; then \
       nodeType=maria \
       nodeTypeSQL=maria \
       userName=root \
-      REPLACEMENT='db.${nodeType}.driverClass=org.${nodeTypeSQL}.Driver\ndb.${nodeType}.url=jdbc:${nodeTypeSQL}://${node.address}/\ndb.${nodeType}.username=${userName}\ndb.${nodeType}.password=${PASSWORD}' \
+      REPLACEMENT='db.${nodeType}.driverClass=org.${nodeTypeSQL}.Driver\ndb.${nodeType}.url=jdbc:${nodeTypeSQL}://${address}/\ndb.${nodeType}.username=${userName}\ndb.${nodeType}.password=${PASSWORD}' \
       sed "s~//StartJel~//StartJel\n${REPLACEMENT}\n~g" ${HOME}/mobileapp-server-api.properties \
   ; fi \
-  ; if [ "`expr index ${node.nodeType} postgres`" == "0" ]; then \
+  ; if [ "`expr index ${nodeType} postgres`" == "0" ]; then \
       nodeType=postgres \
       nodeTypeSQL=postgresql \
       userName=webadmin \
-      REPLACEMENT='db.${nodeType}.driverClass=org.${nodeTypeSQL}.Driver\ndb.${nodeType}.url=jdbc:${nodeTypeSQL}://${node.address}/\ndb.${nodeType}.username=${userName}\ndb.${nodeType}.password=${PASSWORD}' \
+      REPLACEMENT='db.${nodeType}.driverClass=org.${nodeTypeSQL}.Driver\ndb.${nodeType}.url=jdbc:${nodeTypeSQL}://${address}/\ndb.${nodeType}.username=${userName}\ndb.${nodeType}.password=${PASSWORD}' \
       sed "s~//StartJel~//StartJel\n${REPLACEMENT}\n~g" ${HOME}/mobileapp-server-api.properties \
   ; fi \
-; elif [ "cp" == "${node.nodeGroup}" ]; then \
-    REPLACEMENT='nodes.cp.url=${node.url}\nnodes.cp.nodeId=${node.id}\nnodes.cp.address=${node.address}\nnodes.cp.port=${node.port}' \
+; elif [ "cp" == "${nodeGroup}" ]; then \
+    REPLACEMENT='nodes.cp.url=${url}\nnodes.cp.nodeId=${id}\nnodes.cp.address=${address}\nnodes.cp.port=${port}' \
     sed "s~//StartJel~//StartJel\n${REPLACEMENT}\n~g" ${HOME}/mobileapp-server-api.properties \
 ; fi \
-; if [ "bl" == "${node.nodeGroup}" ]; then \
-    REPLACEMENT='nodes.bl.url=${node.url}\nnodes.bl.nodeId=${node.id}\nnodes.bl.address=${node.address}\nnodes.bl.port=${node.port}' \
+; if [ "bl" == "${nodeGroup}" ]; then \
+    REPLACEMENT='nodes.bl.url=${url}\nnodes.bl.nodeId=${id}\nnodes.bl.address=${address}\nnodes.bl.port=${port}' \
     sed "s~//StartJel~//StartJel\n${REPLACEMENT}\n~g" ${HOME}/mobileapp-server-api.properties \
 ; fi \
-; if [ "cache" == "${node.nodeGroup}" ]; then \
-    REPLACEMENT='nodes.${node.nodeType}.name=${node.name}\nnodes.${node.nodeType}.nodeType=${node.nodeType}\nnodes.${node.nodeType}.nodeId=${node.id}\nnodes.${node.nodeType}.address=${node.address}\nnodes.${node.nodeType}.port=${node.port}' \
+; if [ "cache" == "${nodeGroup}" ]; then \
+    REPLACEMENT='nodes.${nodeType}.name=${name}\nnodes.${nodeType}.nodeType=${nodeType}\nnodes.${nodeType}.nodeId=${id}\nnodes.${nodeType}.address=${address}\nnodes.${nodeType}.port=${port}' \
     sed "s~//StartJel~//StartJel\n${REPLACEMENT}\n~g" ${HOME}/mobileapp-server-api.properties \
 ; fi \
-; if [ "nosqldb" == "${node.nodeGroup}" ]; then \
+; if [ "nosqldb" == "${nodeGroup}" ]; then \
     grep -q 'u admin' /var/log/jem.log && grep 'u admin' /var/log/jem.log | grep -o '[^ ]*$' | tail -n1 >> /var/lib/jelastic/keys/password.txt || cat /var/log/jem.log | grep passwd | awk '{print $NF}' >> /var/lib/jelastic/keys/password.txt; \
     PASSWORD=$(< /var/lib/jelastic/keys/password.txt) \
     rm /var/lib/jelastic/keys/password.txt \
-    REPLACEMENT='nodes.${node.nodeType}.name=${node.name}\nnodes.${node.nodeType}.url=${node.url}\\nnodes.${node.nodeType}.nodeId=${node.id}\\nnodes.${node.nodeType}.address=${node.address}\nnodes.${node.nodeType}.port=${node.port}\nnodes.${node.nodeType}.password=${PASSWORD}' \
+    REPLACEMENT='nodes.${nodeType}.name=${name}\nnodes.${nodeType}.url=${url}\\nnodes.${nodeType}.nodeId=${id}\\nnodes.${nodeType}.address=${address}\nnodes.${nodeType}.port=${port}\nnodes.${nodeType}.password=${PASSWORD}' \
     sed "s~//StartJel~//StartJel\n${REPLACEMENT}\n~g" ${HOME}/mobileapp-server-api.properties \
 ; fi
