c55c7e295fb5cb8704fe7d926f40a25e3deb779e
JelasticJPS <ik+1@jelastic.com>
2016-06-07 16:06:14 +0300

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,11 +9,13 @@ name=$7
 
 if [ "sqldb" == "${nodeGroup}" ]; then \
    PASSWORD=`cat /var/log/jem.log | grep "passwd" | awk -F " " '{print $6}' | tail -n1` \
+   echo $PASSWORD \
    ; if [ "`expr index ${nodeType} mysql`" == "0" ]; then \
       nodeType=mysql \
       nodeTypeSQL=mysql \
       userName=root \
       REPLACEMENT='db.${nodeType}.driverClass=org.${nodeTypeSQL}.Driver\ndb.${nodeType}.url=jdbc:${nodeTypeSQL}://${address}/\ndb.${nodeType}.username=${userName}\ndb.${nodeType}.password=${PASSWORD}' \
+      echo $REPLACEMENT \
       sed "s~//StartJel~//StartJel\n${REPLACEMENT}\n~g" ${HOME}/mobileapp-server-api.properties \
    ; fi \
    ; if [ "`expr index ${nodeType} maria`" == "0" ]; then \
@@ -21,6 +23,7 @@ if [ "sqldb" == "${nodeGroup}" ]; then \
       nodeTypeSQL=maria \
       userName=root \
       REPLACEMENT='db.${nodeType}.driverClass=org.${nodeTypeSQL}.Driver\ndb.${nodeType}.url=jdbc:${nodeTypeSQL}://${address}/\ndb.${nodeType}.username=${userName}\ndb.${nodeType}.password=${PASSWORD}' \
+      echo $REPLACEMENT \
       sed "s~//StartJel~//StartJel\n${REPLACEMENT}\n~g" ${HOME}/mobileapp-server-api.properties \
   ; fi \
   ; if [ "`expr index ${nodeType} postgres`" == "0" ]; then \
@@ -28,18 +31,22 @@ if [ "sqldb" == "${nodeGroup}" ]; then \
       nodeTypeSQL=postgresql \
       userName=webadmin \
       REPLACEMENT='db.${nodeType}.driverClass=org.${nodeTypeSQL}.Driver\ndb.${nodeType}.url=jdbc:${nodeTypeSQL}://${address}/\ndb.${nodeType}.username=${userName}\ndb.${nodeType}.password=${PASSWORD}' \
+      echo $REPLACEMENT \
       sed "s~//StartJel~//StartJel\n${REPLACEMENT}\n~g" ${HOME}/mobileapp-server-api.properties \
   ; fi \
 ; elif [ "cp" == "${nodeGroup}" ]; then \
     REPLACEMENT='nodes.cp.url=${url}\nnodes.cp.nodeId=${id}\nnodes.cp.address=${address}\nnodes.cp.port=${port}' \
+    echo cp = $REPLACEMENT
     sed "s~//StartJel~//StartJel\n${REPLACEMENT}\n~g" ${HOME}/mobileapp-server-api.properties \
 ; fi \
 ; if [ "bl" == "${nodeGroup}" ]; then \
     REPLACEMENT='nodes.bl.url=${url}\nnodes.bl.nodeId=${id}\nnodes.bl.address=${address}\nnodes.bl.port=${port}' \
+    echo bl = $REPLACEMENT
     sed "s~//StartJel~//StartJel\n${REPLACEMENT}\n~g" ${HOME}/mobileapp-server-api.properties \
 ; fi \
 ; if [ "cache" == "${nodeGroup}" ]; then \
     REPLACEMENT='nodes.${nodeType}.name=${name}\nnodes.${nodeType}.nodeType=${nodeType}\nnodes.${nodeType}.nodeId=${id}\nnodes.${nodeType}.address=${address}\nnodes.${nodeType}.port=${port}' \
+    echo cache = $REPLACEMENT
     sed "s~//StartJel~//StartJel\n${REPLACEMENT}\n~g" ${HOME}/mobileapp-server-api.properties \
 ; fi \
 ; if [ "nosqldb" == "${nodeGroup}" ]; then \
